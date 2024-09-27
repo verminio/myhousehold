d3c63d88fe428b5b37245eb03493d4423cb87e92
Chris <christopher@burrows.me>
2024-09-27 14:01:35 +0100

    Add expense_types table

---
+++ migrations/1727440836_expense_types.js
@@ -0,0 +1,33 @@
+/// <reference path="../pb_data/types.d.ts" />
+// Create collection for expense types
+migrate((db) => {
+  const dao = new Dao(db);
+  const expenseTypes = new Collection({
+    name: "expense_types",
+    type: "base",
+    listRule:   "@request.auth.id != ''",
+    viewRule:   "@request.auth.id != ''",
+    createRule: "@request.auth.id != ''",
+    updateRule: null,
+    deleteRule: null,
+    schema:     [
+        {
+            name:     "type",
+            type:     "text",
+            required: true,
+            options:  {
+                min: 1,
+            },
+        },
+    ],
+    indexes: [
+        "CREATE UNIQUE INDEX idx_expense_type ON expense_types (type)"
+    ],
+    options: {}
+  });
+  dao.saveCollection(expenseTypes);
+}, (db) => {
+  const dao = new Dao(db);
+  const expenseTypes = dao.findCollectionByNameOrId("expense_types");
+  dao.deleteCollection(expenseTypes);
+})
